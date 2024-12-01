5e71f5e8b3c2db5e00e321ca24ceb7ad757b675f
Stefan Lundkvist <flippah.king@gmail.com>
2024-12-01 21:31:44 +0100

    Add lib folder to git

--- README.md
+++ README.md
@@ -10,11 +10,11 @@ This repository contains solutions for the Advent of Code 2024 challenges.
     cd adventofcode2024
     ```
 
-2. Ensure you have Python 3.8+ installed.
+2. Ensure you have Python 3.13 installed.
 
 3. Install any required dependencies:
     ```sh
-    pip install -r requirements.txt
+    pipenv install
     ```
 
 ## Usage
@@ -25,18 +25,11 @@ python day1.py
 ```
 Replace day1.py with the appropriate day's script.
 
-Structure
-day1.py: Contains the solution for Day 1.
-util.py: Utility functions used across different days.
-test_data: Directory containing test data for each day.
-Example
-To run the solution for Day 1:
-
-This will output the results of solve1 and solve2 for the given input.
-
 ## Links
 
 https://github.com/OroArmor/AdventOfCode/blob/master/src/2024/day1.py
+
+https://pypi.org/project/advent-of-code-data/
 
 ## License
 This project is licensed under the MIT License.
---
+++ lib/point_util.py
@@ -0,0 +1,71 @@
+from enum import Enum
+from typing import Generator, Iterator, Self
+
+
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __getitem__(self, item):
+        if item == 0:
+            return self.x
+        elif item == 1:
+            return self.y
+
+        raise AttributeError(f"Invalid item for getitem: {item}")
+
+    def __add__(self, other):
+        if isinstance(other, Point):
+            return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        if isinstance(other, Point):
+            return Point(self.x + other.x, self.y + other.y)
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            return Point(self.x * other, self.y * other)
+
+    def __truediv__(self, other):
+        if isinstance(other, int):
+            return Point(self.x // other, self.y // other)
+
+    def __hash__(self):
+        return self.x * 31 + self.y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __repr__(self):
+        return str(self)
+
+    def __lt__(self, other):
+        return self.x < other.x or self.x == other.x and self.y < other.y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __neg__(self):
+        return Point(-self.x, -self.y)
+
+    def iterate_internals(self) -> Generator[Iterator[Self], None, None]:
+        for x in range(0, 1 if self.x == 0 else self.x, -1 if self.x < 0 else 1):
+            for y in range(0, 1 if self.y == 0 else self.y, -1 if self.y < 0 else 1):
+                yield Point(x, y)
+
+    def manhattan(self, other: Self = None):
+        if other is None:
+            other = Point(0, 0)
+        return abs(self.x - other.x) + abs(self.y - other.y)
+
+
+class Direction(Point, Enum):
+    RIGHT = 1, 0
+    UP = 0, 1
+    LEFT = -1, 0
+    DOWN = 0, -1
+
+    @classmethod
+    def values(cls):
+        return [Direction.RIGHT, Direction.UP, Direction.LEFT, Direction.DOWN]
